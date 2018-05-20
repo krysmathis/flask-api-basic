5eac71799ac0fd5b0bc0af45bb7515935e457d35
krysmathis <krysmathis@gmail.com>
2018-05-20 09:07:42 -0500

    create database on start

--- create_tables.py
+++ create_tables.py
@@ -1,60 +1,50 @@
 import sqlite3
 
-# Creating the database
-connection = sqlite3.connect('data.db')
+def create_db() : 
+    # Creating the database
+    connection = sqlite3.connect('data.db')
 
-# create the connection
-cursor = connection.cursor()
+    # create the connection
+    cursor = connection.cursor()
 
-# saving this for later
-# drop_table = "DROP TABLE IF EXISTS items"
+    # saving this for later
+    # drop_table = "DROP TABLE IF EXISTS areas"
 
-# cursor.execute(drop_table)
+    # cursor.execute(drop_table)
 
-create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
-# now run the sql
-cursor.execute(create_table)
+    create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+    # now run the sql
+    cursor.execute(create_table)
 
-create_table = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name text, price real)"
-# now run the sql
-cursor.execute(create_table)
+    create_table = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name text, price real)"
+    # now run the sql
+    cursor.execute(create_table)
 
-create_table = "CREATE TABLE IF NOT EXISTS areas (id INTEGER PRIMARY KEY, location text, locid int, capture_date DATE, image BLOB)"
-# now run the sql
-cursor.execute(create_table)
+    create_table = "CREATE TABLE IF NOT EXISTS areas (id INTEGER PRIMARY KEY, location text, locid int, capture_date DATE, image BLOB)"
+    # now run the sql
+    cursor.execute(create_table)
 
-# # run queries
-# delete_query = "DELETE FROM users"
-# cursor.execute(delete_query)
+    # check if location1 exists and if not
+    query = "SELECT * FROM areas WHERE location=?"
+    # parameters have to be in a tuple
+    result = cursor.execute(query, ("location1",))
+    row = result.fetchone()
 
-# user = ('krys', 'asdf')
-# insert_query = "INSERT INTO users (username, password) VALUES (?,?)"
+    if row is None:
+        area = ('location1', 1, '2018-05-20', '01010101')
+        insert_query = ("INSERT INTO areas VALUES (NULL, ?, ?, ?, ?);")
+        cursor.execute(insert_query, area)
 
-# cursor.execute(insert_query, user)
-# cursor.execute("INSERT INTO items VALUES (NULL, 'test', 9.99)")
+    # users = [
+    #     ('rolf', 'asdf'),
+    #     ('anne', 'xyz')
+    # ]
 
-# check if location1 exists and if not
+    # cursor.executemany(insert_query, users)
 
-query = "SELECT * FROM areas WHERE location=?"
-# parameters have to be in a tuple
-result = cursor.execute(query, ("location1",))
-row = result.fetchone()
+    # select_query = "SELECT * FROM users"
+    # for row in cursor.execute(select_query) :
+    #     print(row)
 
-if row is None:
-    area = ('location1', 1, '2018-05-20', '01010101')
-    insert_query = ("INSERT INTO areas VALUES (NULL, ?, ?, ?, ?);")
-    cursor.execute(insert_query, area)
-
-# users = [
-#     ('rolf', 'asdf'),
-#     ('anne', 'xyz')
-# ]
-
-# cursor.executemany(insert_query, users)
-
-# select_query = "SELECT * FROM users"
-# for row in cursor.execute(select_query) :
-#     print(row)
-
-connection.commit()
-connection.close()
+    connection.commit()
+    connection.close()
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import app
+from create_tables import create_db
 
 @app.before_first_request
 def create_tables():
-    
+    create_db()
