32e6f4c7e3c725f059953bc35ba85f327201a366
krysmathis <krysmathis@gmail.com>
2018-05-20 21:44:23 -0500

    refactored area model and resource

---
+++ models/area.py
@@ -0,0 +1,38 @@
+from db import db
+
+class AreaModel(db.Model) : 
+    __tablename__ = 'areas'
+    
+    # location text, locid int, capture_date DATE, image BLOB)"
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(80))
+    locationid = db.Column(db.Integer)
+    capture_date = db.Column(db.Date)
+    image = db.Column(db.String)
+
+    def __init__(self, location, locationid, date_captured, image) : 
+        self.location = location
+        self.locationid = locationid
+        self.date_captured = date_captured
+        self.image = image
+    
+
+    def json(self):
+        return {
+            'location': self.location, 
+            'locationid': self.locationid,
+            'date_captured': self.capture_date,
+            'image': self.image
+            }
+
+    @classmethod
+    def find_by_name(cls, location):
+        return cls.query.filter_by(location=location).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
--- resources/area.py
+++ resources/area.py
@@ -1,7 +1,6 @@
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
-import sqlite3
-
+from models.area import AreaModel
 
 class Area(Resource):
     TABLE_NAME = 'areas'
@@ -21,7 +20,7 @@ class Area(Resource):
         help="This field cannot be left blank!"
     )
     
-    parser.add_argument('date',
+    parser.add_argument('date_captured',
         type=str,
         required=True,
         help="This field cannot be left blank!"
@@ -35,109 +34,40 @@ class Area(Resource):
     @jwt_required()
     def get(self, name):
         
-        area = self.find_by_name(name)
+        area = AreaModel.find_by_name(name)
         if area:
-            return area
+            return area.json()
+            
         return {'message': 'Item not found'}, 404
 
-    @classmethod
-    def find_by_name(cls, location):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+    def post(self, location):
+        if AreaModel.find_by_name(location):
+            return {'message': "An item with area '{}' already exists.".format(location)}
 
-        query = "SELECT * FROM {table} WHERE location=?".format(table=cls.TABLE_NAME)
-        result = cursor.execute(query, (location,))
-        row = result.fetchone()
-        connection.close()
-
-        if row:
-            return {'search': {'location': row[1], 'date_captured': row[3]}}
-
-    def post(self, area):
-        # if self.find_by_name(area):
-        #     return {'message': "An item with area '{}' already exists.".format(area)}
-
-        print(Area.parser)
-        data = Area.parser.parse_args()
+        data = self.parser.parse_args()
 
         # may need to parse again here to convert image to proper sqlite format
-
-        area = {'location': area, 'locationid': data['locationid'], 'captured_date': data['captured_date'], 'image': data['image']}
+        area = AreaModel(location, locationid=data['locationid'], date_captured=data['date_captured'], image=data['image'])
 
         try:
-            Area.insert(area)
+            area.save_to_db()
         except:
             return {"message": "An error occurred inserting the area."}
 
-        return area
+        return area.json()
 
-    @classmethod
-    def insert(cls, area):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO {table} VALUES(NULL, ?, ?, ?, ?)".format(table=cls.TABLE_NAME)
-        cursor.execute(query, (area['location'], area['locationid'], area['captured_date'], area['image']))
-
-        connection.commit()
-        connection.close()
-
+   
     @jwt_required()
-    def delete(self, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+    def delete(self, location):
 
-        query = "DELETE FROM {table} WHERE name=?".format(table=self.TABLE_NAME)
-        cursor.execute(query, (name,))
-
-        connection.commit()
-        connection.close()
-
-        return {'message': 'Item deleted'}
-
-    # @jwt_required()
-    # def put(self, name):
-    #     data = Item.parser.parse_args()
-    #     item = self.find_by_name(name)
-    #     updated_item = {'name': data['name'], 'price': data['price']}
-    #     if item is None:
-    #         try:
-    #             Item.insert(updated_item)
-    #         except:
-    #             return {"message": "An error occurred inserting the item."}
-    #     else:
-    #         try:
-    #             Item.update(updated_item)
-    #         except:
-    #             raise
-    #             return {"message": "An error occurred updating the item."}
-    #     return updated_item
-
-    # @classmethod
-    # def update(cls, item):
-    #     connection = sqlite3.connect('data.db')
-    #     cursor = connection.cursor()
-
-    #     query = "UPDATE {table} SET price=? WHERE name=?".format(table=cls.TABLE_NAME)
-    #     cursor.execute(query, (item['price'], item['name']))
-
-    #     connection.commit()
-    #     connection.close()
+        if AreaModel.find_by_name(location) :
+            AreaModel.delete_from_db()
+            return {'message': 'Item deleted'}
+        
+        return {'message': 'Item not found'}
 
 
 class AreaList(Resource):
-    TABLE_NAME = 'areas'
 
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM {table}".format(table=self.TABLE_NAME)
-        result = cursor.execute(query)
-        areas = []
-        #for row in result:
-            # TODO:
-            #Areas.append({'name': row[1], 'price': row[2]})
-        connection.close()
-
-        return {'areas': areas}
+        return [area.json() for area in AreaModel.query.all()]
